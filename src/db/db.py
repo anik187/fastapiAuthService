7e219c7b3538677741a4e3897608547fb04f8269
Asif Quadir <asif.anik2000@gmail.com>
2024-12-13 22:39:36 +0600

    2nd push

--- src/db/db.py
+++ src/db/db.py
@@ -8,7 +8,6 @@ from fastapi import Depends
 
 load_dotenv(dotenv_path=".env")
 
-# engine = create_engine("mysql+pymysql://root:test-mysql@localhost:3306/fastapi")
 mysql_url = os.getenv("MYSQL_URL")
 engine = create_engine(f"mysql{mysql_url}")
 
@@ -19,11 +18,3 @@ def get_session():
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
-
-# with Session(engine) as session:
-#     test_user = Users(
-#         email="test@test.com", password="1234", refreshToken="aksdjfksdjf"
-#     )
-#     session.add(test_user)
-#     session.commit()
-#     print("test user created successfully")
